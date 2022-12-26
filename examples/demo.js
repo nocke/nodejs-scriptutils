21811a33f22276c125722aa0444f7d6338634ea8
_USER_ <_@_>
2022-12-26 13:42:26 +0100

    “better” main entry pont, added `examples/demo.js`

---
+++ examples/demo.js
@@ -0,0 +1,6 @@
+#!/usr/bin/env node
+
+import {howdy} from '../main.js'
+
+howdy()
+
--- main.js
+++ main.js
@@ -1,10 +1,16 @@
 #!/usr/bin/env node
-
+// TODO 'use strict'
 import { execSync } from 'child_process';
 
-module.exports = function() {
+export const howdy = function() {
     const msg = '\nhowdy, npm package! ♥♥♥'
     console.log(msg)
     execSync(`notify-send -i info '${msg}' -t 4000`)
     return;
 };
+
+export default {
+    howdy
+}
+
+
